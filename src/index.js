b5b153195d3c4897f5a18c1172af0d9201020e57
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-06-19 21:55:20 -0300

    adiciona configuração e deploy para colocar projeto no ar

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import PortfolioProvider from './context/PortfolioProvider';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={ process.env.PUBLIC_URL }>
       <PortfolioProvider>
         <App />
       </PortfolioProvider>
